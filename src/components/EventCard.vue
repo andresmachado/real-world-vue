4d5273421899f87fd7b198668135b0481895b74e
André Machado <csantos.machado@gmail.com>
2019-08-24 10:04:13 -0300

    Adding Axios and Api Service

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,31 +1,20 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <span>{{ event.attendees.length }}</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 22, 2019',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Andre Machado' },
-          { id: 'abc321', name: 'Suelle Machado' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
